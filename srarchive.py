6fa59b78f92476e20812fca893dfc1cd68ab3f77
Anders Dalskov <anderspkd@gmail.com>
2017-12-26 19:38:32 +0100

    Moved everything into one file. More scriptlike.

--- srarchive.py
+++ srarchive.py
@@ -1,14 +1,88 @@
 #!/usr/bin/env python
 
 import argparse
+import requests
 import re
 import time
 import os
 import sys
 import json
+import datetime
 
-from util import log, ERROR, pp_time, WARN
-import bot as _bot
+# Utility functions
+NORMAL = 0
+WARN = 1
+ERROR = 2
+
+
+def log(msg, level=NORMAL, end='\n'):
+    f = sys.stdout
+    if level == NORMAL:
+        pre = '[ ]'
+        f.write(f'{pre} {msg}{end}')
+    elif level == WARN:
+        pre = '[W]'
+        f.write(f'{pre} {msg}{end}')
+    elif level == ERROR:
+        pre = '[E]'
+        f.write(f'{pre} {msg}{end}')
+        exit(1)
+    else:
+        f.write(f'[F] unknown logging level {level}\n')
+        exit(1)
+    f.flush()
+
+
+# Parse UNIX timestamp as something readable.
+def pp_time(timestamp):
+    if timestamp is not None:
+        ts = datetime.datetime.fromtimestamp(
+            timestamp
+        ).strftime('%Y-%m-%d %H:%M:%S')
+    return (ts or 'N/A', timestamp or 'N/A')
+
+
+# Bot stuff
+class AuthenticationError(Exception):
+    pass
+
+
+# A simple bot that can authenticate and perform GET and POST requests
+class Bot:
+
+    def __init__(self, username, password, api_id, api_secret, user_agent):
+        self.usr = username
+        self.pwd = password
+        self.id = api_id
+        self.sec = api_secret
+        self.headers = {'User-Agent': user_agent}
+
+    def post(self, url, **kwargs):
+        return requests.post(url, headers=self.headers, **kwargs)
+
+    def get(self, url, **kwargs):
+        r = requests.get(url, headers=self.headers, **kwargs)
+        return r
+
+    def auth(self):
+        auth = requests.auth.HTTPBasicAuth(self.id, self.sec)
+        data = {
+            'grant_type': 'password',
+            'username': self.usr,
+            'password': self.pwd
+        }
+        resp = self.post(
+            'https://www.reddit.com/api/v1/access_token',
+            auth=auth,
+            data=data
+        )
+        token = resp.json().get('access_token')
+
+        if token is None:
+            raise AuthenticationError(f'Could not authenticate: {resp.json()}')
+
+        self.headers['Authorization'] = f'bearer {token}'
+
 
 # Arguments
 parser = argparse.ArgumentParser(
